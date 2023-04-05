d283fe75f095b9a319152f07c7ecce24fa67c4bb
anilaltuner <anil@firstbatch.xyz>
2023-04-05 13:44:41 +0300

    Fetch rust plugin added

--- warp-contracts-plugin-fetch/src/index.ts
+++ warp-contracts-plugin-fetch/src/index.ts
@@ -1,12 +1,30 @@
-import { WarpPlugin, WarpPluginType } from "warp-contracts";
-import fetch from "node-fetch";
+import { WarpPlugin, WarpPluginType } from 'warp-contracts';
+import 'warp-isomorphic';
+
+const Fetch = async (url: string) => {
+  const response = await fetch(url);
+  return await response.text();
+};
+
+const rustImports = (helpers) => {
+  return {
+    __wbg_Fetch: function (arg0, arg1) {
+      const ret = Fetch(helpers.getStringFromWasm0(arg0, arg1));
+      return helpers.addHeapObject(ret);
+    }
+  };
+};
 
 export class FetchExtension implements WarpPlugin<any, void> {
   process(input: any): void {
-    input.fetch = fetch;
+    input.fetch = {
+      fetch,
+      Fetch,
+      rustImports
+    };
   }
 
   type(): WarpPluginType {
-    return "smartweave-extension-fetch";
+    return 'smartweave-extension-fetch';
   }
 }
