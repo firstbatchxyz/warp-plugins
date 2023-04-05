a2dc483cc4866585b441c495c7768d0dfe95e776
fco-fbatch <farukcan@firstbatch.xyz>
2023-04-05 15:37:44 +0300

    v0.1.5

--- warp-contracts-plugin-fetch/src/index.ts
+++ warp-contracts-plugin-fetch/src/index.ts
@@ -1,5 +1,5 @@
-import { WarpPlugin, WarpPluginType } from 'warp-contracts';
-import 'warp-isomorphic';
+import { WarpPlugin, WarpPluginType } from "warp-contracts";
+import "warp-isomorphic";
 
 const rust_fetch = async (url: string) => {
   const response = await fetch(url);
@@ -11,7 +11,7 @@ const rustImports = (helpers) => {
     __wbg_rustfetch: function (arg0, arg1) {
       const ret = rust_fetch(helpers.getStringFromWasm0(arg0, arg1));
       return helpers.addHeapObject(ret);
-    }
+    },
   };
 };
 
@@ -20,11 +20,11 @@ export class FetchExtension implements WarpPlugin<any, void> {
     input.fetch = {
       fetch,
       rust_fetch,
-      rustImports
+      rustImports,
     };
   }
 
   type(): WarpPluginType {
-    return 'smartweave-extension-fetch';
+    return "smartweave-extension-fetch";
   }
 }
